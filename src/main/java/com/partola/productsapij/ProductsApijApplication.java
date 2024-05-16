74daae3da909f44ac347d47ac9a2b3d44df16eda
partolaaa <ivan.partola77@gmail.com>
2024-05-16 18:55:10 +0200

    Init commit

---
+++ src/main/java/com/partola/productsapij/ProductsApijApplication.java
@@ -0,0 +1,13 @@
+package com.partola.productsapij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsApijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partola/productsapij/ProductsApijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partola.productsapij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
